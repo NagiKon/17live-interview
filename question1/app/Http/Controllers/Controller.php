1cc83042e494cb984c14002e638b2ab2a1c08516
ChengHan Li <chenghanli@applyeasypro.com>
2022-01-26 19:41:08 +0800

    [feat] 新增自定義回傳格式

--- question1/app/Http/Controllers/Controller.php
+++ question1/app/Http/Controllers/Controller.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Exceptions\ValidationException;
+
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
@@ -10,4 +12,29 @@ use Illuminate\Routing\Controller as BaseController;
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    public function successFormat($data = [])
+    {
+        return response()->json([
+            'status' => 'success',
+            'data' => $data,
+        ], 200);
+    }
+
+    public function failFormat($error, $httpCode = 500)
+    {
+        return response()->json([
+            'status' => 'error',
+            'error' => $error
+        ], $httpCode);
+    }
+
+    public function handleDefaultValidatorException($validator)
+    {
+        if ($validator->fails()) {
+            $errorMessages = $validator->errors()->getMessages();
+            $firstErrorField = array_key_first($errorMessages);
+            throw new ValidationException(ValidationException::ERROR_FIELD, $firstErrorField, $errorMessages[$firstErrorField][0]);
+        }
+    }
 }
