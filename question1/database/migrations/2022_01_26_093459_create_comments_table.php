2581d32b5b542509dd343259ac4c5ae5bf7f45e6
ChengHan Li <chenghanli@applyeasypro.com>
2022-01-26 17:42:26 +0800

    [feat] 新增 comments 表用的 model 與 migration 類別

---
+++ question1/database/migrations/2022_01_26_093459_create_comments_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCommentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comments', function (Blueprint $table) {
+            $table->id()->comment('留言 ID');
+            $table->bigInteger('post_id')->comment('文章 ID');
+            $table->text('message')->comment('留言內容');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('Comment');
+    }
+}
